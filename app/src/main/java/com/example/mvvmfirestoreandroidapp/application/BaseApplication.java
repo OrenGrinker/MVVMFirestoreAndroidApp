b1e21de94cf3046b61e7cd96f1f99f40bee61a60
Oren Grinker <46677615+OrenGrinker@users.noreply.github.com>
2021-11-18 17:09:52 +0200

    Update BaseApplication.java

--- app/src/main/java/com/example/mvvmfirestoreandroidapp/application/BaseApplication.java
+++ app/src/main/java/com/example/mvvmfirestoreandroidapp/application/BaseApplication.java
@@ -1,2 +1,19 @@
-package com.example.mvvmfirestoreandroidapp.application;public class BaseApplication {
+package com.example.mvvmfirestoreandroidapp.application;
+
+import android.app.Application;
+import com.example.mvvmfirestoreandroidapp.BuildConfig;
+import timber.log.Timber;
+
+public class BaseApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+            Timber.d("Init Timber for better logging");
+        }
+    }
+
 }
